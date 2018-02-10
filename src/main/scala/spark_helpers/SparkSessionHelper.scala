7b0f364c35dc70613cd126535d9d44652c53ad7c
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-02-10 22:40:19 +0100

    create Spark Session builder

---
+++ src/main/scala/spark_helpers/SparkSessionHelper.scala
@@ -0,0 +1,27 @@
+package spark_helpers
+
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.SparkSession
+
+object SparkSessionHelper {
+  def buildSession(): SparkSession = {
+
+    val sparkMaster = ConfigFactory.load().getString("spark.conf.master.value")
+
+    println("Spark Master: ", sparkMaster)
+
+    val spark: SparkSession = SparkSession.builder
+      .appName("Spark Application")
+      .master(sparkMaster)
+      .enableHiveSupport()
+      .getOrCreate
+
+    spark.sparkContext.setLogLevel("INFO")
+
+    spark
+  }
+
+  def getSparkSession(): SparkSession ={
+    SparkSession.builder().getOrCreate()
+  }
+}
