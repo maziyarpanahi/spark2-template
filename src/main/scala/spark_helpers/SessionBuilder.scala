b7e4c4adb17d91a1a7a3ba7d148617c043ad4052
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-12-18 13:39:16 +0100

    Change spark session helper

---
+++ src/main/scala/spark_helpers/SessionBuilder.scala
@@ -0,0 +1,49 @@
+package spark_helpers
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018. Maziyar Panahi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import org.apache.spark.sql.SparkSession
+
+object SessionBuilder {
+  def buildSession(): SparkSession = {
+
+//    Logger.getLogger("com").setLevel(Level.ERROR)
+//    Logger.getLogger("org").setLevel(Level.ERROR)
+//    Logger.getLogger("akka").setLevel(Level.ERROR)
+//    LogManager.getRootLogger.setLevel(Level.ERROR)
+
+    val spark: SparkSession = SparkSession.builder
+      .appName("spar2-template")
+      .master("local[*]")
+      .config("spark.driver.memory", "4G")
+      .config("spark.kryoserializer.buffer.max","200M")
+      .config("spark.serializer","org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate
+
+    spark.sparkContext.setLogLevel("INFO")
+    // export sparkSession
+    spark
+  }
+}
